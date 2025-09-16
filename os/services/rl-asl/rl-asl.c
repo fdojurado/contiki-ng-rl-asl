41045c336ed031a89bbdb11fee06d8822da95dcf
Fernando J <fdo.jurado@gmail.com>
2025-09-16 15:14:06 -0500

    feat(rl-asl): add configuration header and refactor skip RX logic

--- os/services/rl-asl/rl-asl.c
+++ os/services/rl-asl/rl-asl.c
@@ -1,23 +1,35 @@
 #include "rl-asl.h"
-#include <stdlib.h> 
+#include <stdlib.h>
+#include "rl-asl-conf.h"
 
 /* log */
 #include "sys/log.h"
 #define LOG_MODULE "rl-asl"
 #define LOG_LEVEL LOG_CONF_LEVEL_RL_ASL
 
-static uint64_t counter = 0;
-
 void rl_asl_check_skip_rx(const struct tsch_link *link, bool *skip_rx)
 {
-    // /Every ten consecutive slots, skip the RX slot if the link has RX option
-    if ((link->link_options & LINK_OPTION_RX) && ((counter % 100) == 0))
+    uint16_t sf_handle = link->slotframe_handle;
+    if (sf_handle != RL_ASL_UNICAST_SLOTFRAME_HANDLE)
     {
         *skip_rx = false;
+        return;
     }
-    else
+
+    if (tsch_is_associated == 0)
     {
-        *skip_rx = true;
+        *skip_rx = false;
+        return;
     }
-    counter++;
+
+    // /Every ten consecutive slots, skip the RX slot if the link has RX option
+    // if ((link->link_options & LINK_OPTION_RX) && ((counter % 100) == 0))
+    // {
+    //     *skip_rx = false;
+    // }
+    // else
+    // {
+    //     *skip_rx = true;
+    // }
+    // counter++;
 }
