1f04d5b6fb45bace732b864f2f81a8c70c0fa250
Fernando J <fdo.jurado@gmail.com>
2025-09-16 15:07:17 -0500

    feat(rl-asl): update skip RX logic to skip every 100 slots instead of random

--- os/services/rl-asl/rl-asl.c
+++ os/services/rl-asl/rl-asl.c
@@ -6,19 +6,18 @@
 #define LOG_MODULE "rl-asl"
 #define LOG_LEVEL LOG_CONF_LEVEL_RL_ASL
 
+static uint64_t counter = 0;
+
 void rl_asl_check_skip_rx(const struct tsch_link *link, bool *skip_rx)
 {
-    // Lets flip a coin to decide whether to skip RX or not for demonstration purposes
-    if (link->link_options & LINK_OPTION_RX)
+    // /Every ten consecutive slots, skip the RX slot if the link has RX option
+    if ((link->link_options & LINK_OPTION_RX) && ((counter % 100) == 0))
     {
-        *skip_rx = (rand() % 2) == 0; // 50% chance to skip RX
+        *skip_rx = false;
     }
     else
     {
-        *skip_rx = false; // Do not skip RX if the link does not have RX option
+        *skip_rx = true;
     }
-    LOG_DBG("Link handle: %u, RX option: %s, Skip RX: %s\n",
-            link->handle,
-            (link->link_options & LINK_OPTION_RX) ? "Yes" : "No",
-            *skip_rx ? "Yes" : "No");
+    counter++;
 }
