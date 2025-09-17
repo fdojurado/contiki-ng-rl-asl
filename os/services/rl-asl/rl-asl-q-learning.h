12a8cd6540cf90bbdc5db2e26abac65f2b7ba71b
Fernando J <fdo.jurado@gmail.com>
2025-09-17 07:55:39 -0500

    refactor(rl-asl): add reward and penalty definitions for Q-Learning

--- os/services/rl-asl/rl-asl-q-learning.h
+++ os/services/rl-asl/rl-asl-q-learning.h
@@ -21,6 +21,11 @@
 #define RL_ASL_B_INTERARRIVAL 4
 #define RL_ASL_B_ASN 3
 
+#define REWARD_RX_TX          10.0
+#define REWARD_SKIP_RX_NO_TX   1.0
+#define PENALTY_RX_NO_TX     -1.0
+#define PENALTY_SKIP_RX_TX   -10.0
+
 #define RL_ASL_NUM_STATES (RL_ASL_B_LISTEN * RL_ASL_B_REWARD * RL_ASL_B_INTERARRIVAL * RL_ASL_B_ASN)
 
 // enum actions
