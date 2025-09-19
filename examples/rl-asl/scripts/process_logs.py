24624edcf1bd127b92cf5a5beda01519467cfd34
Fernando J <fdo.jurado@gmail.com>
2025-09-18 23:01:06 -0500

    fix(process_logs): correct regex pattern for UC Idle Radio Rx

--- examples/rl-asl/scripts/process_logs.py
+++ examples/rl-asl/scripts/process_logs.py
@@ -53,7 +53,7 @@ _RE_ENERGEST_RADIO_RX_TIME = re.compile(
 _RE_ENERGEST_RADIO_UC_RX_TIME = re.compile(
     r"\[INFO: Energest\s*\]\s*UC Radio Rx\s*:\s*(\d+)")
 _RE_ENERGEST_RADIO_UC_IDLE_RX_TIME = re.compile(
-    r"\[INFO: Energest\s*\]\s*UC Idle Radio Rx\s*:\s*(\d+)")
+    r"\[INFO: Energest\s*\]\s*UC Idle Rx\s*:\s*(\d+)")
 _RE_ENERGEST_RADIO_UC_IDLE_RATIO = re.compile(
     r"\[INFO: Energest\s*\]\s*UC Idle ratio\s*\([\d\.]+\s*\)\s*:\s*([\d\.]+)")
 _RE_ENERGEST_RADIO_UC_RATIO = re.compile(
